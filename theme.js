28829a369f178a84f47170080bccba0fc5a4290c
Rayyan <63111246+Ryyn-Sd@users.noreply.github.com>
2020-08-01 19:03:22 -0400

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const themes = [ "default", "default_repocard", "dark", "radical", "merko", "gruvbox", "tokyonight", "onedark", "cobalt", "synthwave", "highcontrast", "dracula", "prussian", "monokai", "vue", "shades-of-purple", "nightowl", "buefy", "blue-green", "algolia", "great-gatsby" ];
+const themes = [ "dark", "radical", "merko", "gruvbox", "tokyonight", "onedark", "cobalt", "synthwave", "highcontrast", "dracula", "prussian", "monokai", "shades-of-purple", "nightowl", "blue-green", "algolia", "great-gatsby" ];
 const theme = themes[Math.floor(Math.random() * themes.length)];
 fs.readFile('src/README.md', 'utf8', function (err,data) {
   if (err) {
